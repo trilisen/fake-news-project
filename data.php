2778e71c5cb9f08bb1318167fd146ca39a3860b5
Trilisen <jonmcg01@gmail.com>
2020-10-21 14:55:08 +0200

    started on generateContent()

--- data.php
+++ data.php
@@ -4,3 +4,13 @@ declare(strict_types=1);
 
 // This is the file where you can keep your data arrays such as articles and
 // authors.
+
+$authors = [
+    ['fullName' => 'William Shakespeare', 'id' => 1],
+    ['fullName' => 'Emily Dickinson', 'id' => 2],
+    ['fullName' => 'Isaac Asimov', 'id' => 3],
+    ['fullName' => 'John Steinbeck', 'id' => 4],
+    ['fullName' => 'Alexandre Dumas', 'id' => 5]
+];
+
+$items = [];
--- functions.php
+++ functions.php
@@ -2,5 +2,13 @@
 
 declare(strict_types=1);
 
+require __DIR__ . '/data.php';
+
+
 // This is the file where you can keep all your functions. Remember to NOT
 // execute/run any functions in this file. Keep it dumb.
+
+function generateContent($title, $content, $author, $published, $likes)
+{
+    $items[] = ['title' => $title, 'content' => $content, 'author' = ''];
+};
