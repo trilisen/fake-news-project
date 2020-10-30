d3abcc24c921ae61d7ec662c96527ad146b02753
Trilisen <jonmcg01@gmail.com>
2020-10-31 00:36:38 +0100

    generateRandomDate() added

--- functions.php
+++ functions.php
@@ -6,3 +6,20 @@ require __DIR__ . '/data.php';
 
 // This is the file where you can keep all your functions. Remember to NOT
 // execute/run any functions in this file. Keep it dumb.
+
+
+function generateRandomDate()
+{
+    // 1577836800 == 01/01/2020 @ 12:00am (UTC)
+    // 1604016000 == 30/10/2020 @ 12:00am (UTC) 
+
+    $date = rand(1577836800, 1604016000);
+
+    return $date;
+};
+
+function sortArticles(array $posts)
+{
+    usort($posts, function ($a, $b) {
+    });
+}
