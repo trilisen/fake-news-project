57f3ab4bcb9b422673d1483898143645c8b1b1e8
Trilisen <jonmcg01@gmail.com>
2020-11-02 00:56:36 +0100

    added header.php file

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Piazzolla:wght@400;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/style.css">
+    <title>The Rabbits Carrot</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="header-title">The Rabbits Carrot</h1>
+    </header>
+
+    <main>
