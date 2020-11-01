3dbf1f8bc38d7e2db73df441f2720b1842c73efd
Trilisen <jonmcg01@gmail.com>
2020-11-02 00:57:27 +0100

    moved foreach lopp into main tag and moved header into seperate file

--- index.php
+++ index.php
@@ -5,55 +5,36 @@
 // of the files containing HTML or even better; in another PHP file altogether.
 require __DIR__ . '/functions.php';
 require __DIR__ . '/data.php';
+require __DIR__ . '/header.php'
+?>
+
+<?php foreach (sortArticles($posts) as $post) :
+
+    $title = $post['title'];
+    $content = $post['content'];
+    $author = getAuthorById($post['author_id'], $authors);
+    $date = date('Y-m-d', $post['published_date']);
+    $likes = $post['likes'];
+    $img = $post['picture'];
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Piazzolla:wght@400;800&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/style.css">
-    <title></title>
-</head>
-
-<body>
-    <header>
-        <h1 class="header-title">The Rabbits Carrot</h1>
-    </header>
-
-
-    <?php foreach (sortArticles($posts) as $post) :
-
-        $title = $post['title'];
-        $content = $post['content'];
-        $author = getAuthorById($post['author_id'], $authors);
-        $date = date('Y-m-d', $post['published_date']);
-        $likes = $post['likes'];
-        $img = $post['picture'];
-
-    ?>
-        <main>
-            <article>
-                <section>
-                    <h1><?= $title ?></h1>
-                    <div class="likes">
-                        <p><?= $likes ?></p>
-                        <img src="/Thumb_up_icon_2.svg" alt="thumb-up">
-                    </div>
-                </section>
-                <img src="<?= $img ?>" alt="" class="main-picture">
-                <p class="main-content"> <?= $content ?></p>
-                <div class="main-footer">
-                    <p><?= $author ?></p>
-                    <p>Published: <?= $date ?></p>
-                </div>
-            </article>
-
-        </main>
-    <?php endforeach; ?>
+    <article>
+        <section>
+            <h1><?= $title ?></h1>
+            <div class="likes">
+                <p><?= $likes ?></p>
+                <img src="/Thumb_up_icon_2.svg" alt="thumb-up">
+            </div>
+        </section>
+        <img src="<?= $img ?>" alt="" class="main-picture">
+        <p class="main-content"> <?= $content ?></p>
+        <div class="main-footer">
+            <p><?= $author ?></p>
+            <p>Published: <?= $date ?></p>
+        </div>
+    </article>
+<?php endforeach; ?>
+</main>
 </body>
 
 </html>
